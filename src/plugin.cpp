9a02ac50e6185b81e9af4ff82642b3d04e303ef1
stain2003 <2981327456@qq.com>
2025-01-27 20:59:31 +0800

    1111

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,38 +1,62 @@
-#include "log.h"
+//#include "log.h"
+//
+//
+//void OnDataLoaded()
+//{
+//   
+//}
+//
+//void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+//{
+//	switch (a_msg->type) {
+//	case SKSE::MessagingInterface::kDataLoaded:
+//        
+//		break;
+//	case SKSE::MessagingInterface::kPostLoad:
+//		break;
+//	case SKSE::MessagingInterface::kPreLoadGame:
+//		break;
+//	case SKSE::MessagingInterface::kPostLoadGame:
+//        break;
+//	case SKSE::MessagingInterface::kNewGame:
+//		break;
+//	}
+//}
+//
+//SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+//    SKSE::Init(skse);
+//	SetupLog();
+//
+//	//SKSE::GetPapyrusInterface()->Register(BindPapyrusFunctions);
+//
+//    /*auto messaging = SKSE::GetMessagingInterface();
+//	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+//		return false;
+//	}*/
+//
+//	
+//    return true;
+//}
+//
+//
 
-
-void OnDataLoaded()
-{
-   
+RE::Actor* GetNPCFormIDByEditorID(RE::StaticFunctionTag*, std::string EditorID) {
+    return RE::TESForm::LookupByEditorID<RE::Actor>(EditorID);
 }
 
-void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+std::string TestingPrint(RE::StaticFunctionTag*)
 {
-	switch (a_msg->type) {
-	case SKSE::MessagingInterface::kDataLoaded:
-        
-		break;
-	case SKSE::MessagingInterface::kPostLoad:
-		break;
-	case SKSE::MessagingInterface::kPreLoadGame:
-		break;
-	case SKSE::MessagingInterface::kPostLoadGame:
-        break;
-	case SKSE::MessagingInterface::kNewGame:
-		break;
-	}
+	return "testing script";
 }
 
-SKSEPluginLoad(const SKSE::LoadInterface *skse) {
-    SKSE::Init(skse);
-	SetupLog();
+bool PapyrusFunction(RE::BSScript::IVirtualMachine* vm) {
+    vm->RegisterFunction("GetNPCFormIDByEditorID", "SkyRomanceMiscFunction", GetNPCFormIDByEditorID);
+	vm->RegisterFunction("TestingPrint", "SkyRomanceMiscFunction", TestingPrint);
+	return true;
+}
 
-
-    auto messaging = SKSE::GetMessagingInterface();
-	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
-		return false;
-	}
-
-	
-    return true;
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+	SKSE::Init(skse);
+	SKSE::GetPapyrusInterface()->Register(PapyrusFunction);
+	return true;
 }
